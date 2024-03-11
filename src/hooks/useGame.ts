5da2479cf5076a6f47dde14bdefaf3090208023c
yanyan90 <yanik_lemieux@hotmail.com>
2024-03-11 09:48:11 -0400

    make Loading Card skeleton

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -27,21 +27,26 @@ export interface Game {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isloading,setLoading] = useState(false)
   
     // recuperer tout les games / jeux video
     useEffect(() => {
 const controller = new AbortController()
-
+setLoading(true)
       apiClient
         .get<FetchGamesResponse>("/games", {signal: controller.signal})
-        .then((res) => setGames(res.data.results))
+        .then((res) => {
+          setGames(res.data.results);
+          setLoading(false);})
         .catch((err) => {
         if (err instanceof CanceledError) return;
-        setError(err.message)});
+        setError(err.message)
+        setLoading(false)
+    });
 
         return ()=> controller.abort()
     },[]);
-    return { games, error }
+    return { games, error , isloading }
 }
 
 export default useGames;
